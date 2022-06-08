4372211dcfd0316ac1b5ec3fbd33398c29b07b46
Antonio Jose Tirado <75416812+Synchrer@users.noreply.github.com>
2022-06-08 10:08:12 +0200

    Modificado Config (Annadidos items finales)

--- synch_chemicals/shared/config.lua
+++ synch_chemicals/shared/config.lua
@@ -11,34 +11,45 @@ labBlips = {
 
 chemicalPureItems = {
     --{element = , chemicalSymbol = ""},
-    {element = "hydrogen", chemicalSymbol = "H", value = 0}, 
-    {element = "nitrogen", chemicalSymbol = "N", value = 0}, 
-    {element = "sulfur", chemicalSymbol = "S", value = 0},
-    {element = "oxygen", chemicalSymbol = "O", value = 0},
-    {element = "agua_quimica", chemicalSymbol = "Na", value = 0},
-    {element = "chloride", chemicalSymbol = "Cl", value = 0},
-    {element = "carbon", chemicalSymbol = "C", value = 0},
-    {element = "potassium", chemicalSymbol = "K", value = 0},
+    {element = "carbon", chemicalSymbol = "C", value = 0}, -- 67
+    {element = "chloride", chemicalSymbol = "Cl", value = 0}, -- 67.1
+    {element = "hydrogen", chemicalSymbol = "H", value = 0}, -- 72
+    {element = "potassium", chemicalSymbol = "K", value = 0}, -- 75
+    {element = "nitrogen", chemicalSymbol = "N", value = 0}, -- 78
+    {element = "sodium", chemicalSymbol = "Na", value = 0}, -- 78.1
+    {element = "oxygen", chemicalSymbol = "O", value = 0}, -- 79
+    {element = "sulfur", chemicalSymbol = "S", value = 0}, -- 83
+    {element = "zinc", chemicalSymbol = "Z", value = 0}, -- 90
 }
 
--- el campo components tiene el valor asci de la primera letra de cada elemento, ordenados N-78 H-72 Na-78 O-79 C-67 Cl-67 S-83 K-75
-  
+-- el campo components tiene el valor asci de la primera letra de cada elemento
 chemicalCompoundItems = {
-    --{compound = "", chemicalSymbol = "", components = {}},
-    {compound = "nitrogenHydride", chemicalSymbol = "N+H^3", components = {72,78}}, -- H, N
-    {compound = "hydrogenOxide", chemicalSymbol = "H^2+O", components = {72,79}}, -- H, O
-    {compound = "prueba", chemicalSymbol = "N^2+H", components = {72,78.1}}, -- H, Na
-    {compound = "sodiumChloride", chemicalSymbol = "Na+Cl", components = {67,78.1}}, -- Na, Cl
-    {compound = "nitricAcid", chemicalSymbol = "H+N+O^3", components = {72,78,79}}, -- H, N, O
-    {compound = "oleicAcid", chemicalSymbol = "C^18+H^34-O^2", components = {67,72,79}}, -- C, H, O
-    {compound = "sodiumHydroxide", chemicalSymbol = "Na+O+H", components = {72,78.1,79}}, -- Na, O, H
-    {compound = "sulfuricAcid", chemicalSymbol = "H^2+S+O^4", components = {72,79,83}}, -- H, S, O
-    {compound = "sulfurousAcid", chemicalSymbol = "H^2+S+O^3", components = {72,79,83}}, -- H, S, O
-    {compound = "sodiumCarbonate", chemicalSymbol = "Na^2+C+O^3", components = {67,78.1,79}}, -- Na, C, O
-    {compound = "potassiumNitrate", chemicalSymbol = "K+N+O^3", components = {75,78,79}}, -- K, N, O
+    {compound = "nitricacid", chemicalSymbol = "H+N+O^3", components = {72,78,79}}, -- H, N, O
+    {compound = "sulfuricacid", chemicalSymbol = "H^2+S+O^4", components = {72,79,83}}, -- H, S, O
+    {compound = "potassiumnitrate", chemicalSymbol = "K+N+O^3", components = {75,78,79}}, -- K, N, O
+    {compound = "sal_quimica", chemicalSymbol = "Na+Cl", components = {67.1,78.1}}, -- Na, Cl
+    {compound = "mezclapolvora", chemicalSymbol = "S+C", components = {67,83}}, -- S, C
+    {compound = "agua_quimica", chemicalSymbol = "H^2+O", components = {72,79}}, -- H, O
+    {compound = "etanol", chemicalSymbol = "C^2+H^5+O", components = {67,72,79}}, -- C, H, O
+    {compound = "sodiumhydroxide", chemicalSymbol = "Na+O+H", components = {72,78.1,79}}, -- Na, O, H
+    {compound = "ethyl", chemicalSymbol = "C^8+H^9", components = {67,72}}, -- C, H
+    {compound = "nitrogendioxyde", chemicalSymbol = "N+O^2", components = {78,79}}, -- N, O
+    {compound = "basepegamento", chemicalSymbol = "Z^2+H^35+O^2", components = {72,79,90}}, -- Z, H, O
+
 }
 
---crear nuevo array
+finalItems = {
+    --{final = ""},
+    {final = "polvora"}, -- potassiumNitrate + mezclaPolvora
+    {final = "betadine"}, -- sodiumHydroxide + agua_quimica
+    {final = "alcohol"}, -- etanol
+    {final = "analgesicos"}, -- ethyl + nitrogenDioxyde
+    {final = "adhesivo"}, -- agua_quimica + basePegamento
+    {final = "gasmechero"}, -- ethyl
+    {final = "baseantiquemaduras"}, -- agua_quimica + etanol
+    {final = "basejet"}, -- sal_quimica + nitricAcid + sulfuricAcid
+    {final = "fertilizantequimico"}, -- potassiumNitrate
+}
 
 options = {
     ['tanque_quimico'] = {
