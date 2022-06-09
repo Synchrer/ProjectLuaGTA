e15a17402aa13f6700d83d0b8cc0927f01163d8a
Antonio Jose Tirado <75416812+Synchrer@users.noreply.github.com>
2022-06-09 08:44:29 +0200

    Finalizado Server (Annadido borrado de materiales del inventario, annadir materiales al inventario y modificar el nivel del jugador)

--- synch_chemicals/server/main.lua
+++ synch_chemicals/server/main.lua
@@ -9,13 +9,19 @@ end
 
 for k,v in pairs(chemicalPureItems) do 
     ESX.RegisterUsableItem(v.element, function(source)
+        local src = source
+        local xPlayer = ESX.GetPlayerFromId(src)
         TriggerClientEvent('io_chemicals:addPureElement', source, v.element)
+        xPlayer.removeInventoryItem(v.element, 1)
     end)
 end
 
 for k,v in pairs(chemicalCompoundItems) do 
     ESX.RegisterUsableItem(v.compound, function(source)
+        local src = source
+        local xPlayer = ESX.GetPlayerFromId(src)
         TriggerClientEvent('io_chemicals:addCompound', source, v.compound)
+        xPlayer.removeInventoryItem(v.compound, 1)
     end)
 end
 
@@ -40,4 +46,24 @@ RegisterServerEvent("io_chemicals:removeItem")
 AddEventHandler("io_chemicals:removeItem", function(item_name)
     local _source = source
     exports.ox_inventory:RemoveItem(source, item_name, 1)
+end)
+
+RegisterServerEvent("io_chemicals:addItem")
+AddEventHandler("io_chemicals:addItem", function(item_name, quantity)
+    print(item_name, quantity)
+    local src = source
+    local xPlayer = ESX.GetPlayerFromId(src)
+    xPlayer.addInventoryItem(item_name, quantity)
+end)
+
+RegisterServerEvent('io_chemicals:addChemicalLvl')
+AddEventHandler('io_chemicals:addChemicalLvl', function(puntos)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    MySQL.Async.fetchAll('SELECT * FROM `habilidades` WHERE `identifier` = @identifier', {['@identifier'] = xPlayer.identifier}, function(skillInfo)
+		MySQL.Async.execute('UPDATE `habilidades` SET `quimica` = @quimica WHERE `identifier` = @identifier', {
+            ['@quimica'] = (skillInfo[1].quimica + puntos),
+			['@identifier'] = xPlayer.identifier
+		})
+	end)
+    TriggerClientEvent("pNotify:SendNotification", source, {text = "Has adquirido habilidades de quimica. Puntos adquiridos: " .. puntos, type = "info", timeout = 4000, layout = "centerLeft"})
 end)
